d0b9344c958daa13de38a30f1fa62cca0b9b5c93
Justin C. Miller <justin@devjustinian.com>
2020-02-23 19:27:51 -0800

    Fix header include issues
    - Add types.h to all generated proto headers
    - Add tables.h to loaded_image protocol header

--- generate_protos.py
+++ generate_protos.py
@@ -16,6 +16,7 @@ template = Template('''#pragma once
 // https://github.com/justinian/j6-uefi-headers for more information.
 
 #include <uefi/guid.h>
+#include <uefi/types.h>
 {%- for header in headers %}
 #include <{{ header }}>
 {%- endfor %}
